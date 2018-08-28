97467177e232daf67af458dcb752a225226e14d9
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-28 00:14:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 function lowerCaseDrivers(drivers) {
   return drivers.map ( driver => driver.toLowerCase());
 }
+
+function nameToAttributes() {
+  return drivers.map ( function (driver) {
+    names = driver.split(" ")
+    Object.assign({}, {firstName: names[0]}, {lastName: names[1]} )
+  });
+}
