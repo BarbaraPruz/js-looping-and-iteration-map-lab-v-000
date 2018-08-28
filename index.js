544ec87a77f0f19bb22120861651b7d0e3a7bee6
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-28 00:31:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function nameToAttributes(drivers) {
     return Object.assign({}, {firstName: names[0]}, {lastName: names[1]} )
   });
 }
+
+function attributesToPhrase(drivers) {
+  return drivers.map ( function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
